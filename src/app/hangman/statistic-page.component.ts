12490ea479526c41e3a7479a3d774535035c5e52
Marco Florio <marco.florio@mni.thm.de>
2017-02-07 17:35:36 +0100

    added comments and improved function for empty singleplayer topics (like multiplayer)

--- src/app/hangman/statistic-page.component.ts
+++ src/app/hangman/statistic-page.component.ts
@@ -10,11 +10,13 @@ import {Game} from "./game.model";
   templateUrl: 'statistic-page.component.html',
 })
 export class StatisticPageComponent {
+  // counter for wins/losses
   private sp_wins: number = 0;
   private sp_losses: number = 0;
   private mp_wins: number = 0;
   private mp_losses: number = 0;
 
+  // dictionaries for saving topicName and their recpective amounts
   private topicListSP: {[tName: string]: number;} = {}
   private topicListMP: {[tName: string]: number;} = {}
 
@@ -36,63 +38,68 @@ export class StatisticPageComponent {
                                                             "#f586ed", "#f586ed", "#45a73a", "#d1e734",
                                                             "#7a9efa", "#a85903", "#64da85", "#ccc67b"]}];
 
-  // Single Player
+  // Single Player (initialized later)
   private doughnutChartLabelsSP: string[] = [];
   private doughnutChartDataSP: number[] = [];
-  // Multi Player
+  // Multi Player (initialized later)
   private doughnutChartDataMP: number[] = [];
   private doughnutChartLabelsMP: string[] = [];
 
   constructor(private userDataService: UserDataService, public nav: NavController, public alertCtrl: AlertController) {
-    userDataService.findGames().subscribe(
+    userDataService.findGames().subscribe(                // iterate over all games of user
       (games: Game[]) => {
         games.forEach((game) => {
-          if (game.singleplayer) {
-            if (game.topic != null) {
+          if (game.singleplayer) {                        // SinglePlayer
+            if (game.topic != null) {                     // topic=undefined -> invalid
               if (game.topic in this.topicListSP) {
-                this.topicListSP[game.topic]++;
+                this.topicListSP[game.topic]++;           // if topic exists increment
               } else {
-                this.topicListSP[game.topic] = 1;
+                this.topicListSP[game.topic] = 1;         // else create new record in dict
               }
             }
-            if (game.badChars <= 9) {
+            if (game.badChars <= 9) {                     // convention: loss at > 9
               this.sp_wins++;
             } else {
               this.sp_losses++;
             }
-          } else if (game.badCharsOpponent != null) {  // check if MP-Game
-            if (game.topic in this.topicListMP) {
-              this.topicListMP[game.topic]++;
-            } else {
-              this.topicListMP[game.topic] = 1;
-            }
-            if (game.badChars < game.badCharsOpponent) {
-              this.mp_wins++;
-            } else {
-              this.mp_losses++;
+          } else if (game.badCharsOpponent != null) {     // MultiPlayer (only if badChars != null the game is finished)
+            if (game.topic != null) {                     // topic=undefined -> invalid
+              if (game.topic in this.topicListMP) {
+                this.topicListMP[game.topic]++;           // if topic exists increment
+              } else {
+                this.topicListMP[game.topic] = 1;         // else create new record in dict
+              }
+              if (game.badChars < game.badCharsOpponent) {// if your badChars < opponent.badChars -> win
+                this.mp_wins++;
+              } else {
+                this.mp_losses++;
+              }
             }
           }
         });
       }
     );
 
-    // Overview
-    this.barChartData = [
+    this.barChartData = [                                         // load countet wins/losses into barChart
       {data: [this.sp_wins, this.mp_wins], label: 'Siege'},
       {data: [this.sp_losses, this.mp_losses], label: 'Niederlagen'}
     ];
-    // Single Player Topics
-    for (var key in this.topicListSP) {
+
+    // Singleplayer
+    for (var key in this.topicListSP) {                           // load Single Player Topics
       this.doughnutChartLabelsSP.push(key)
       this.doughnutChartDataSP.push(this.topicListSP[key])
     }
-
-    // Multi Player Topics
-    for (var key in this.topicListMP) {
+    if(this.doughnutChartDataSP.length == 0) {                    // if no topics are found
+      this.doughnutChartLabelsSP.push('Noch keine Spiele')
+      this.doughnutChartDataSP.push(0)
+    }
+    // MultiPlayer
+    for (var key in this.topicListMP) {                           // load Multi Player Topics
       this.doughnutChartLabelsMP.push(key)
       this.doughnutChartDataMP.push(this.topicListMP[key])
     }
-    if(this.doughnutChartDataMP.length == 0) {
+    if(this.doughnutChartDataMP.length == 0) {                    // if no topics are found
       this.doughnutChartLabelsMP.push('Noch keine Spiele')
       this.doughnutChartDataMP.push(0)
     }

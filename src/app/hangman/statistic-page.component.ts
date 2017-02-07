74e144d343da2a23424a610b51fcc5a00754d99a
Marco Florio <marco.florio@mni.thm.de>
2017-02-07 17:15:43 +0100

    statistics-page edited to one barchart and two doughnutcharts

--- src/app/hangman/statistic-page.component.ts
+++ src/app/hangman/statistic-page.component.ts
@@ -1,39 +1,103 @@
 import {
   NavController,
-  LoadingController,
   AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { AuthService } from './auth.service';
 import {UserDataService} from "./userdata.service";
+import {Game} from "./game.model";
 
 @Component({
   selector: 'statistic-page',
   templateUrl: 'statistic-page.component.html',
 })
 export class StatisticPageComponent {
-  username:string;
-  constructor(private userDataService: UserDataService,public nav: NavController, public authData: AuthService, public formBuilder: FormBuilder,
-              public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
-    this.username=userDataService.username;
-  }
+  private sp_wins: number = 0;
+  private sp_losses: number = 0;
+  private mp_wins: number = 0;
+  private mp_losses: number = 0;
 
-  // Doughnut
-  private doughnutChartLabels:string[] = ['Siege', 'Niederlagen'];
-  private doughnutChartType:string = 'doughnut';
+  private topicListSP: {[tName: string]: number;} = {}
+  private topicListMP: {[tName: string]: number;} = {}
+
+  // Bar Charts
+  private barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  private barChartLabels: string[] = ['SinglePlayer', 'MultiPlayer'];
+  private barChartType: string = 'bar';
+  private barChartLegend: boolean = true;
+  private barChartData: any[] = [];
+  private barChartColors: any[] = [{backgroundColor: ["#fd4545", "#969696"]}];
+
+  // Doughnut Charts
+  private doughnutChartType: string = 'doughnut';
+  // some random colors generated for 12 different topics
+  private doughnutChartColors: any[] =  [{backgroundColor: ["#7a9efa", "#b698b2", "#94dde7", "#0db59e",
+                                                            "#f586ed", "#f586ed", "#45a73a", "#d1e734",
+                                                            "#7a9efa", "#a85903", "#64da85", "#ccc67b"]}];
 
   // Single Player
-  private doughnutChartDataSP:number[] = [26, 3];
-  private doughnutChartColorsSP: any[] = [{ backgroundColor: ["#fd4545", "#969696"], hoverBackgroundColor : ["#ea2a2a", "#565353"] }];
-
+  private doughnutChartLabelsSP: string[] = [];
+  private doughnutChartDataSP: number[] = [];
   // Multi Player
-  private doughnutChartDataMP:number[] = [14, 33];
-  private doughnutChartColorsMP: any[] = [{ backgroundColor: ["#fd4545", "#969696"], hoverBackgroundColor : ["#ea2a2a", "#565353"] }];
+  private doughnutChartDataMP: number[] = [];
+  private doughnutChartLabelsMP: string[] = [];
 
-  // events
-  public chartClicked(e:any):void {
-  }
+  constructor(private userDataService: UserDataService, public nav: NavController, public alertCtrl: AlertController) {
+    userDataService.findGames().subscribe(
+      (games: Game[]) => {
+        games.forEach((game) => {
+          console.log(game);
+          if (game.singleplayer) {
+            if (game.topic != null) {
+              if (game.topic in this.topicListSP) {
+                this.topicListSP[game.topic]++;
+              } else {
+                this.topicListSP[game.topic] = 1;
+              }
+            }
+            if (game.badChars <= 9) {
+              this.sp_wins++;
+            } else {
+              this.sp_losses++;
+            }
+          } else if (game.badCharsOpponent != null) {  // check if MP-Game
+            if (game.topic in this.topicListMP) {
+              this.topicListMP[game.topic]++;
+            } else {
+              this.topicListMP[game.topic] = 1;
+            }
+            if (game.badChars < game.badCharsOpponent) {
+              this.mp_wins++;
+            } else {
+              this.mp_losses++;
+            }
+          }
+        });
+      }
+    );
 
-  public chartHovered(e:any):void {
+    // Overview
+    this.barChartData = [
+      {data: [this.sp_wins, this.mp_wins], label: 'Siege'},
+      {data: [this.sp_losses, this.mp_losses], label: 'Niederlagen'}
+    ];
+    // Single Player Topics
+    for (var key in this.topicListSP) {
+      this.doughnutChartLabelsSP.push(key)
+      this.doughnutChartDataSP.push(this.topicListSP[key])
+    }
+
+    // Multi Player Topics
+    for (var key in this.topicListMP) {
+      this.doughnutChartLabelsMP.push(key)
+      this.doughnutChartDataMP.push(this.topicListMP[key])
+    }
+    console.log(this.doughnutChartDataMP.length)
+    console.log(this.doughnutChartDataMP)
+    if(this.doughnutChartDataMP.length == 0) {
+      this.doughnutChartLabelsMP.push('Noch keine Spiele')
+      this.doughnutChartDataMP.push(0)
+    }
   }
 }
